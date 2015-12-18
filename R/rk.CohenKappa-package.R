6840828bc42b8e0811700d57ffa4ec2368ce3897
unDocUMeantIt <m@reaktanz.de>
2015-12-18 19:19:08 +0100

    prep for release
      - update build script, ChangeLog and rebuilt

--- R/rk.CohenKappa-package.R
+++ R/rk.CohenKappa-package.R
@@ -3,8 +3,8 @@
 #' \tabular{ll}{
 #' Package: \tab rk.CohenKappa\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.01-7\cr
-#' Date: \tab 2015-03-14\cr
+#' Version: \tab 0.01-8\cr
+#' Date: \tab 2015-11-11\cr
 #' Depends: \tab rkward (>= 0.6.0)\cr
 #' Enhances: \tab rkward\cr
 #' License: \tab GPL (>= 3)\cr
